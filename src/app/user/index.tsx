168e1fb99ffcc0e8144befbdd5b935e4fccb3e72
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-03-05 18:56:12 +0200

    fixing safe areas. setting up bottom navigation and user view

---
+++ src/app/user/index.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+import { Page } from "@/src/components/page";
+import { Text } from "@/src/components/ui/text";
+
+const User = () => {
+  return (
+    <Page>
+      <View>
+        <Text>User Here</Text>
+      </View>
+    </Page>
+  );
+};
+
+export default User;
--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,6 +1,15 @@
 import type { PropsWithChildren } from "react";
 import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export const Page = (props: PropsWithChildren) => {
-  return <View className="flex-1 bg-neutral-100 gap-4 p-4" {...props} />;
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      className="flex-1 bg-neutral-100 gap-4 p-4"
+      style={{ paddingTop: insets.top }}
+      {...props}
+    />
+  );
 };
