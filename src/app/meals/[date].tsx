27b4d73d2b0034696e34aef1ec380f6dea4730d5
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-04-30 04:06:10 +0300

    moved components based on routing changes

--- src/app/meals/[date].tsx
+++ src/app/meals/[date].tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 import { Fragment } from "react";
-import { DailyMeals } from "@/src/components/home/daily-meals";
+import { DailyMeals } from "@/src/components/meals/daily-meals";
 import { Page } from "@/src/components/ui/page";
 import { useParsedLocalParams } from "@/src/hooks/useParsedLocalParams";
 import { pathDateSchema } from "@/src/schemas/search-params";
