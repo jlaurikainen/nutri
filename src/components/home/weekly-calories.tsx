a39837c31144977d5f89e59d4ba27d4f0070e0ca
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-04-30 10:43:21 +0300

    tuned weekly chart

--- src/components/home/weekly-calories.tsx
+++ src/components/home/weekly-calories.tsx
@@ -1,17 +1,15 @@
 import { Fragment } from "react";
 import { View } from "react-native";
 import Svg, { Line, Rect, Text as SVGText } from "react-native-svg";
-import { toDateMonthString } from "@/src/lib/date";
 import { formatNumber } from "@/src/lib/number";
 import { useWeeklyCaloriesSummary } from "./useWeeklyCaloriesSummary";
 
-const BAR_Y_OFFSET = 720,
-  BAR_WIDTH = 180,
+const BAR_WIDTH = 180,
   CHART_HEIGHT = 900,
   CHART_WIDTH = 1600,
-  LEGEND_X_OFFSET = 150,
-  LEGEND_Y = 760,
   MAX_CALORIES = 3000,
+  K_LINE = CHART_HEIGHT / (MAX_CALORIES / 1000),
+  KK_LINE = 2 * K_LINE,
   VALUE_GAP = 32,
   VALUE_X_OFFSET = BAR_WIDTH / 2;
 
@@ -26,6 +24,24 @@ export function WeeklyCalories() {
         width="100%"
       >
         <Rect fill="#181818" height="100%" width="100%" x={0} y={0} />
+        <Line
+          stroke="#666"
+          strokeDasharray={25}
+          strokeWidth={3}
+          x1={0}
+          x2="100%"
+          y1={K_LINE}
+          y2={K_LINE}
+        />
+        <Line
+          stroke="#666"
+          strokeDasharray={25}
+          strokeWidth={3}
+          x1={0}
+          x2="100%"
+          y1={KK_LINE}
+          y2={KK_LINE}
+        />
         {Object.entries(weeklyCalories).map(([key, value], i, self) => (
           <Fragment key={key}>
             <SVGText
@@ -38,42 +54,24 @@ export function WeeklyCalories() {
                 VALUE_X_OFFSET
               }
               y={
-                BAR_Y_OFFSET -
-                ((BAR_Y_OFFSET / MAX_CALORIES) * value + VALUE_GAP)
+                CHART_HEIGHT -
+                ((CHART_HEIGHT / MAX_CALORIES) * value + VALUE_GAP)
               }
             >
               {formatNumber(value, 0)}
             </SVGText>
             <Rect
               fill="#eee"
-              height={(BAR_Y_OFFSET / MAX_CALORIES) * value}
+              height={(CHART_HEIGHT / MAX_CALORIES) * value}
               width={BAR_WIDTH}
               x={
                 i * (CHART_WIDTH / self.length) +
                 (CHART_WIDTH / self.length / 2 - BAR_WIDTH / 2)
               }
-              y={BAR_Y_OFFSET - (BAR_Y_OFFSET / MAX_CALORIES) * value}
+              y={CHART_HEIGHT - (CHART_HEIGHT / MAX_CALORIES) * value}
             />
-            <SVGText
-              fill="#fff"
-              fontSize={50}
-              textAnchor="end"
-              transform={`rotate(-60, ${i * (CHART_WIDTH / self.length) + LEGEND_X_OFFSET}, ${LEGEND_Y})`}
-              x={i * (CHART_WIDTH / self.length) + LEGEND_X_OFFSET}
-              y={LEGEND_Y}
-            >
-              {toDateMonthString(new Date(key))}
-            </SVGText>
           </Fragment>
         ))}
-        <Line
-          stroke="#fff"
-          strokeWidth={5}
-          x1={0}
-          x2="100%"
-          y1={720}
-          y2={720}
-        />
       </Svg>
     </View>
   );
