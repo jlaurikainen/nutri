0802b57641a2b1c0b6034f919a6e1a80e412ba66
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-05-02 14:57:08 +0300

    display average daily calories again

--- src/components/home/weekly-calories.tsx
+++ src/components/home/weekly-calories.tsx
@@ -1,8 +1,18 @@
+import { View } from "react-native";
+import { formatNumber } from "@/src/utils/number";
 import { useWeeklyCaloriesSummary } from "../../hooks/useWeeklyCaloriesSummary";
 import { BarChart } from "../shared/bar-chart";
+import { Text } from "../shared/text";
 
 export function WeeklyCalories() {
-  const { weeklyCalories } = useWeeklyCaloriesSummary();
+  const { weekAverage, weeklyCalories } = useWeeklyCaloriesSummary();
 
-  return <BarChart values={weeklyCalories} />;
+  return (
+    <View className="gap-1">
+      <BarChart values={weeklyCalories} />
+      <Text className="text-center text-sm">
+        Average daily calories: {formatNumber(weekAverage, 0)}kcal
+      </Text>
+    </View>
+  );
 }
