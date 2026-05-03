a3419d588e0a845aad45b22a23ea24345232cea6
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-05-03 11:55:43 +0300

    update user type

--- src/queries/user.ts
+++ src/queries/user.ts
@@ -4,12 +4,21 @@ import z from "zod";
 import { USER_KEY } from "./keys";
 
 const userSchema = z.object({
-  activity: z.number(),
+  activity: z.union([
+    z.literal(0),
+    z.literal(1),
+    z.literal(2),
+    z.literal(3),
+    z.literal(4),
+    z.literal(5),
+  ]),
   age: z.number(),
   height: z.number(),
   id: z.number(),
-  sex: z.number(),
+  sex: z.union([z.literal(0), z.literal(1)]),
 });
+
+export type User = z.infer<typeof userSchema>;
 
 export const useUser = () => {
   const db = useSQLiteContext();
