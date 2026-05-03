a80c92e88b89a02382f700badac930f177b16ae2
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-05-03 17:33:38 +0300

    display bmr limit in weekly calories

--- src/components/home/weekly-calories.tsx
+++ src/components/home/weekly-calories.tsx
@@ -1,15 +1,20 @@
 import { View } from "react-native";
+import { useUserBMR } from "@/src/queries/user";
 import { formatNumber } from "@/src/utils/number";
 import { useWeeklyCaloriesSummary } from "../../hooks/useWeeklyCaloriesSummary";
 import { BarChart } from "../shared/bar-chart";
 import { Text } from "../shared/text";
 
 export function WeeklyCalories() {
+  const bmr = useUserBMR();
   const { weekAverage, weeklyCalories } = useWeeklyCaloriesSummary();
 
   return (
     <View className="gap-1">
-      <BarChart values={weeklyCalories} />
+      <Text className="text-center text-sm">
+        BMR: {formatNumber(bmr, 0)}kcal at Dashed Line
+      </Text>
+      <BarChart comparisonValue={bmr} values={weeklyCalories} />
       <Text className="text-center text-sm">
         Average daily calories: {formatNumber(weekAverage, 0)}kcal
       </Text>
