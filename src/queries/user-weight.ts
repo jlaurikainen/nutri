adc61ba84d4df439f237eb67c231a2ae8ce95114
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-05-03 17:31:27 +0300

    added query hooks for getting user weight data from a time range and for adding a new weight measurement

--- src/queries/user-weight.ts
+++ src/queries/user-weight.ts
@@ -1,7 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSQLiteContext } from "expo-sqlite";
 import z from "zod";
-import { USER_WEIGHT_KEY } from "./keys";
+import { toTimezoneAwareISOStringWithTime } from "../utils/date";
+import { USER_WEIGHT_KEY, USER_WEIGHTS_KEY } from "./keys";
 
 const userWeightSchema = z.object({
   date: z.string(),
@@ -14,8 +15,51 @@ export const useUserWeight = () => {
 
   return useQuery({
     queryFn: async () =>
-      await db.getFirstAsync("SELECT * FROM user_weights ORDER BY date DESC"),
+      await db.getFirstAsync("SELECT * FROM user_weights ORDER BY date DESC;"),
     queryKey: [USER_WEIGHT_KEY],
     select: userWeightSchema.parse,
+  });
+};
+
+export const useUserWeights = (props: { end: Date; start: Date }) => {
+  const db = useSQLiteContext();
+  const startString = toTimezoneAwareISOStringWithTime(props.start);
+  const endString = toTimezoneAwareISOStringWithTime(props.end);
+
+  return useQuery({
+    queryFn: async () =>
+      await db.getAllAsync(
+        `
+          SELECT * FROM user_weights
+          WHERE date BETWEEN ? and ?
+          ORDER BY date DESC;
+        `,
+        [startString, endString],
+      ),
+    queryKey: [USER_WEIGHTS_KEY],
+    select: z.array(userWeightSchema).parse,
+  });
+};
+
+export const useAddWeightMeasurement = () => {
+  const client = useQueryClient();
+  const db = useSQLiteContext();
+
+  return useMutation({
+    mutationFn: async (weight: number) =>
+      await db.runAsync(
+        `
+          INSERT INTO user_weights (
+            date,
+            weight
+          )
+          VALUES (date('now'), ?);
+        `,
+        [weight],
+      ),
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: [USER_WEIGHT_KEY] });
+      client.invalidateQueries({ queryKey: [USER_WEIGHTS_KEY] });
+    },
   });
 };
