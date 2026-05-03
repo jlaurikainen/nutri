54832132ca660eac3c76aebfed81e4d3005cce31
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-05-03 11:56:27 +0300

    list user data and wip bmr without weight data

--- src/app/user/index.tsx
+++ src/app/user/index.tsx
@@ -3,15 +3,45 @@ import { Fragment } from "react";
 import { Page } from "@/src/components/shared/page";
 import { Text } from "@/src/components/shared/text";
 import { useUser } from "@/src/queries/user";
+import { formatNumber } from "@/src/utils/number";
+import { calculateBMR } from "@/src/utils/user";
+
+const ACTIVITY_TO_READABLE = {
+  0: "Sedentary",
+  1: "Exercise 1-3 times/week",
+  2: "Exercise 4-5 times/week",
+  3: "Daily exercise",
+  4: "Intense daily exercise",
+  5: "Very intense daily exercise",
+} as const;
+
+const SEX_TO_READABLE = {
+  0: "Male",
+  1: "Female",
+} as const;
 
 const User = () => {
-  const { data } = useUser();
+  const { data: user } = useUser();
+
+  const userBMR = calculateBMR(user, 70);
 
   return (
     <Fragment>
       <Stack.Screen options={{ title: "User" }} />
       <Page>
-        <Text>{JSON.stringify(data, null, 2)}</Text>
+        {!user ? null : (
+          <Fragment>
+            <Text>Age: {user.age}</Text>
+            <Text>Sex: {SEX_TO_READABLE[user.sex]}</Text>
+            <Text>Activity: {ACTIVITY_TO_READABLE[user.activity]}</Text>
+            <Text>Height: {user.height}</Text>
+          </Fragment>
+        )}
+        <Text>
+          {!user
+            ? "Missing user data to calculate BMR."
+            : `BMR: ${formatNumber(userBMR)}kcal`}
+        </Text>
       </Page>
     </Fragment>
   );
