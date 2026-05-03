6b1a8ebd1262882a41724be4e57113e5ce7db272
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-05-03 17:32:03 +0300

    query keys

--- src/queries/keys.ts
+++ src/queries/keys.ts
@@ -3,3 +3,5 @@ export const MULTI_TEMPLATE_KEY = "meal-templates";
 export const MEALS_KEY = "meals";
 export const USER_KEY = "user";
 export const USER_WEIGHT_KEY = "user-weight";
+export const USER_WEIGHTS_KEY = "user-weights";
+export const USER_BMR = "user-bmr";
