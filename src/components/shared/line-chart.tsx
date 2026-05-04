729811c37e0e4b6fbf8b7d4a5fbceab2c03ed917
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-05-04 11:25:57 +0300

    add line chart wip for showing weight data

---
+++ src/components/shared/line-chart.tsx
@@ -0,0 +1,98 @@
+import { Fragment } from "react";
+import { View } from "react-native";
+import Svg, {
+  Circle,
+  Line,
+  Polyline,
+  Rect,
+  Text as SVGText,
+} from "react-native-svg";
+
+const VIEWBOX_HEIGHT = 900,
+  VIEWBOX_WIDTH = 1600;
+
+interface Props {
+  values: Record<string, number>;
+}
+
+export const LineChart = (props: Props) => {
+  const values = Object.values(props.values);
+
+  const maxValue = Math.max(...values);
+  const heightOffset = maxValue * 2;
+  const widthSafeAreaOffset = values.length === 1 ? VIEWBOX_WIDTH / 2 : 100;
+
+  return (
+    <View className="aspect-video border border-foreground">
+      <Svg
+        height="100%"
+        viewBox={`0 0 ${VIEWBOX_WIDTH} ${VIEWBOX_HEIGHT}`}
+        width="100%"
+      >
+        <Rect fill="#f7fff7" height="100%" id="bg" width="100%" x={0} y={0} />
+
+        <Polyline
+          fill="none"
+          points={values
+            .map(
+              (v, i) =>
+                `${widthSafeAreaOffset + i * ((VIEWBOX_WIDTH - widthSafeAreaOffset * 2) / Math.max(values.length - 1, 1))} ${VIEWBOX_HEIGHT - (VIEWBOX_HEIGHT / heightOffset) * v}`,
+            )
+            .join(", ")}
+          stroke="#4a4a4a"
+          strokeWidth={6}
+        />
+
+        {Object.entries(props.values).map(([k, v], i, self) => {
+          const pointX =
+            widthSafeAreaOffset +
+            i *
+              ((VIEWBOX_WIDTH - widthSafeAreaOffset * 2) /
+                (self.length - 1 || 1));
+          const pointY = VIEWBOX_HEIGHT - (VIEWBOX_HEIGHT / heightOffset) * v;
+
+          return (
+            <Fragment key={k}>
+              <Circle cx={pointX} cy={pointY} fill="#4a4a4a" r={20} />
+
+              <SVGText
+                fill="#1c1c1e"
+                fontFamily="SpaceGrotesk-Regular"
+                fontSize={50}
+                textAnchor="middle"
+                x={pointX}
+                y={100}
+              >
+                {v}
+              </SVGText>
+
+              <Line
+                stroke="#4e4e4e"
+                strokeDasharray={10}
+                strokeWidth={5}
+                x1={pointX}
+                x2={pointX}
+                y1={150}
+                y2={VIEWBOX_HEIGHT - 150}
+              />
+
+              <SVGText
+                fill="#1c1c1e"
+                fontFamily="SpaceGrotesk-Regular"
+                fontSize={50}
+                textAnchor="middle"
+                x={pointX}
+                y={VIEWBOX_HEIGHT - 60}
+              >
+                {new Date(k).toLocaleDateString("fi", {
+                  day: "numeric",
+                  month: "numeric",
+                })}
+              </SVGText>
+            </Fragment>
+          );
+        })}
+      </Svg>
+    </View>
+  );
+};
