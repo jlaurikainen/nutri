05c0c34d46afccec0b0e2bd935f02a97a60d0a53
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-05-04 10:31:46 +0300

    add wip route for user weight measurements

---
+++ src/app/user/user-weight.tsx
@@ -0,0 +1,12 @@
+import { Page } from "@/src/components/shared/page";
+import { Text } from "@/src/components/shared/text";
+
+const UserWeight = () => {
+  return (
+    <Page>
+      <Text>User Weight Here</Text>
+    </Page>
+  );
+};
+
+export default UserWeight;
