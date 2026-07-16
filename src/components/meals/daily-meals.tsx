1396a27a8f1d157056e4cf5995d26f1ab84197b3
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-07-16 15:10:30 +0300

    added total macros to daily meals

--- src/components/meals/daily-meals.tsx
+++ src/components/meals/daily-meals.tsx
@@ -1,8 +1,6 @@
-import { Trash2 } from "lucide-react-native";
-import { ScrollView, View } from "react-native";
+import { Pressable, ScrollView, View } from "react-native";
 import { useDeleteMeal, useMeals } from "@/src/queries/meals";
-import { Button } from "../shared/button";
-import { Icon } from "../shared/icon";
+import { reduceToDailyMacros } from "@/src/utils/macros";
 import { MealItem } from "../shared/meal-item";
 import { Text } from "../shared/text";
 
@@ -16,6 +14,8 @@ export const DailyMeals = (props: Props) => {
     end: props.date ?? new Date(),
     start: props.date ?? new Date(),
   });
+
+  const macros = reduceToDailyMacros(data);
 
   const onDelete = (id: number) => {
     return () => {
@@ -34,24 +34,50 @@ export const DailyMeals = (props: Props) => {
   return (
     <ScrollView>
       <View className="gap-2">
+        <View className="mb-4 gap-1">
+          <Text className="text-xl">Total Macros</Text>
+          <View className="flex-row justify-between border-light-gray border-b">
+            <Text>Calories:</Text>
+            <Text>{macros.calories}kcal</Text>
+          </View>
+          <View className="flex-row justify-between border-light-gray border-b">
+            <Text>Carbs:</Text>
+            <Text>{macros.carbs}g</Text>
+          </View>
+          <View className="flex-row justify-between border-light-gray border-b">
+            <Text>Sugar:</Text>
+            <Text>{macros.sugar}g</Text>
+          </View>
+          <View className="flex-row justify-between border-light-gray border-b">
+            <Text>Fat:</Text>
+            <Text>{macros.fat}g</Text>
+          </View>
+          <View className="flex-row justify-between border-light-gray border-b">
+            <Text>Protein:</Text>
+            <Text>{macros.protein}g</Text>
+          </View>
+          <View className="flex-row justify-between">
+            <Text>Fiber:</Text>
+            <Text>{macros.fiber}g</Text>
+          </View>
+        </View>
+
         {data.map((x) => (
-          <MealItem key={x.id}>
-            <MealItem.Heading
-              action={
-                <Button onPress={onDelete(x.id)} size="icon" variant="bordered">
-                  <Icon as={Trash2} />
-                </Button>
-              }
-            >
-              <MealItem.Title title={x.name} />
-            </MealItem.Heading>
-            <MealItem.Macros>
-              <MealItem.Macro label="Calories" unit="kcal" value={x.calories} />
-              <MealItem.Macro label="Carbs" unit="g" value={x.carbs} />
-              <MealItem.Macro label="Protein" unit="g" value={x.protein} />
-              <MealItem.Macro label="Fat" unit="g" value={x.fat} />
-            </MealItem.Macros>
-          </MealItem>
+          <Pressable key={x.id} onLongPress={onDelete(x.id)}>
+            <MealItem>
+              <MealItem.Heading>
+                <MealItem.Title title={x.name} />
+              </MealItem.Heading>
+              <MealItem.Macros>
+                <MealItem.Macro label="calories" value={x.calories} />
+                <MealItem.Macro label="carbs" unit="g" value={x.carbs} />
+                <MealItem.Macro label="sugar" unit="g" value={x.sugar} />
+                <MealItem.Macro label="fat" unit="g" value={x.fat} />
+                <MealItem.Macro label="protein" unit="g" value={x.protein} />
+                <MealItem.Macro label="fiber" unit="g" value={x.fiber} />
+              </MealItem.Macros>
+            </MealItem>
+          </Pressable>
         ))}
       </View>
     </ScrollView>
