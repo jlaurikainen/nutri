2afa1b7e114c80b6ddde457976c72aa9841f0b58
Juha Laurikainen <jmlaurikainen@gmail.com>
2026-07-16 12:58:17 +0300

    attempted to clean up weekly calories summary hook further

--- src/hooks/useWeeklyCaloriesSummary.ts
+++ src/hooks/useWeeklyCaloriesSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useMeals } from "@/src/queries/meals";
 import {
   addDays,
@@ -9,37 +10,29 @@ import {
 
 const TODAY = endOfDay(new Date());
 const WEEK_AGO = startOfDay(addDays(TODAY, -6));
-const WEEKLY_CALORIES = new Array(difference(WEEK_AGO, TODAY))
-  .fill(null)
-  .reduce((a: Record<string, number>, _c, i) => {
-    const dateString = toTimezoneAwareISOString(addDays(WEEK_AGO, i));
-
-    if (!a[dateString]) {
-      a[dateString] = 0;
-    }
-
-    return a;
-  }, {});
 
 export const useWeeklyCaloriesSummary = () => {
   const { data = [] } = useMeals({ end: TODAY, start: WEEK_AGO });
 
-  const weeklyCalories = Object.entries(
-    data.reduce((a, c) => {
-      if (!(c.date in a)) {
-        return a;
-      }
+  const weeklyCalories = useMemo(() => {
+    const weekMap = new Map<string, number>();
 
-      a[c.date] += c.calories;
+    /** Init all days for the weekly range of dates */
+    for (let i = 0; i < difference(WEEK_AGO, TODAY); i++) {
+      weekMap.set(toTimezoneAwareISOString(addDays(WEEK_AGO, i)), 0);
+    }
 
-      return a;
-    }, WEEKLY_CALORIES),
-  ).map(([key, value]) => ({ calories: value, date: key }));
+    for (const meal of data) {
+      const currentCalories = weekMap.get(meal.date) ?? 0;
+      weekMap.set(meal.date, currentCalories + meal.calories);
+    }
 
-  const nonEmptyDayCalories = weeklyCalories.filter((x) => x.calories > 0);
-  const weeklyAverage =
-    nonEmptyDayCalories.reduce((a, c) => (a += c.calories), 0) /
-    (nonEmptyDayCalories.length || 1);
+    return [...weekMap].map(([date, calories]) => ({ calories, date }));
+  }, [data]);
+
+  const nonEmptyDays = weeklyCalories.filter((x) => x.calories > 0);
+  const totalCalories = nonEmptyDays.reduce((a, c) => (a += c.calories), 0);
+  const weeklyAverage = totalCalories / (nonEmptyDays.length || 1);
 
   return { weeklyAverage, weeklyCalories };
 };
